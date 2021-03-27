9a25d9f3159d85feccb9d2ae3bf54f3ce423e23e
root <jdtorres@unillanos.edu.co>
2021-03-27 00:50:35 -0500

    make it betterxd

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php include('conexionDB.php');
 ?>
 
+
 <!doctype html>
 <html lang="es">
 <meta charset="UTF -8" />
-<body background="https://1.bp.blogspot.com/-QV5qvP2D8tI/XU0DQIEpZrI/AAAAAAAAOac/Q_4qLkpkcKsN2HUUhUeO5q2hSMt_aXaqQCLcBGAs/s640/Tour-de-Francia-nuevo-logotipo-02.png">
+    
 <head>
-<title>TOUR DE FRANCIA 2021</title>
+    <title>TOUR DE FRANCIA 2021</title>
+    <link rel="stylesheet" type="text/css" href="estiloInicio.css" />
 </head>
 
-<title>App Tour de Francia</title>
-<h1>Tour De Francia 2021</h1>
-<h2>Daniel Alferez - Juan Torres</h2>
+<body>
+    <div class="cabecera">
+        <h1><center><font face="Comic Sans MS">APP TOUR DE FRANCIA</font></center></h1>
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a href="#" style="text-decoration: none;">Inicio</a></li>
+            <li><a href="#" style="text-decoration: none;">Equipos</a></li>
+            <li><a href="#" style="text-decoration: none;">Clasificaciones</a></li>
+        </ul>
+    </nav>
+    
+    
+</body>
+
 </html>
