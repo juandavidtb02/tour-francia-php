c66d018d4589323475f6928d2b7c3468ace95c00
root <jdtorres@unillanos.edu.co>
2021-03-26 23:11:16 -0500

    make it betterxd

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php include('conexionDB.php');
 ?>
 
-<html>
+<!doctype html>
+<html lang="es">
+<meta charset="UTF -8" />
+
+<head>
+<title>TOUR DE FRANCIA 2021</title>
+</head>
+
 <title>App Tour de Francia</title>
 <h1>Tour De Francia 2021</h1>
 <h2>Daniel Alferez - Juan Torres</h2>
