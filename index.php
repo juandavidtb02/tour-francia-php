762266347281bb0fd582ad24e55ea446879210e2
root <jdtorres@unillanos.edu.co>
2021-03-30 17:07:29 -0500

    make it betterxd

--- index.php
+++ index.php
@@ -12,40 +12,37 @@
 </head>
 
 <body>
-    <div class="cabecera">
-        <h1><center><font face="Comic Sans MS">APP TOUR DE FRANCIA</font></center></h1>
-        <img src="https://i1.wp.com/mcusercontent.com/23e248de0b05fce198ede786a/images/3754bb62-1be2-4ab4-ba10-a4e3afb94864.png?w=1170&ssl=1">
-    </div>
-    <nav class="menu">
-        <ul>
-            <li><a href="#">Inicio</a></li>
-            <li>
-                <a href="#">Equipos</a>
-                <ul>
-                    <li><a href="#">Equipos participantes</a></li>
-                    <li><a href="#">Ciclistas participantes</a></li>
-                    <li><a href="#">Paises participantes</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Clasificaciones</a>
-                <ul>
-                    <li><a href="#">Clasificacion general</a></li>
-                    <li><a href="#">Clasificacion de Sprint</a></li>
-                    <li><a href="#">Clasificacion por montaña</a></li>
-                    <li><a href="#">Clasificacion por puntos</a></li>
-                    <li><a href="#">Clasificacion por equipos</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Etapas</a>
-                <ul>
-                    <li><a href="#">Recorrido 2021</a></li>
-                    <li><a href="#">Ganadores por etapa</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
+    <header>
+        <h1>TOUR DE FRANCIA 2021</h1>
+        <nav id="menu">
+            <ul>
+                <li id="item"><a href="#">Inicio</a></li>
+                <li id="item"><a href="#">Equipos</a>
+                    <ul id="desple">
+                        <li><a href="#">Equipos participantes</a></li>
+                        <li><a href="#">Ciclistas participantes</a></li>
+                        <li><a href="#">Paises participantes</a></li>
+                    </ul>
+                </li>
+                <li id="item"><a href="#">Clasificaciones</a>
+                    <ul id="desple2">
+                        <li><a href="#">Clasificacion general</a></li>
+                        <li><a href="#">Clasificacion de Sprint</a></li>
+                        <li><a href="#">Clasificacion por montaña</a></li>
+                        <li><a href="#">Clasificacion por puntos</a></li>
+                        <li><a href="#">Clasificacion por equipos</a></li>
+                    </ul>
+                </li>
+                <li id="item"><a href="#">Etapas</a>
+                    <ul id="desple3">
+                        <li><a href="#">Recorrido 2021</a></li>
+                        <li><a href="#">Ganadores por etapas</a></li>
+                    </ul>
+                </li>
+            </ul>
+        
+        </nav>
+    </header>
     
 </body>
 
