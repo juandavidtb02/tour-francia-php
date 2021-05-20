48bf20a8c7f9189278ae6441024dd476f6a7ff90
root <dcalferez@unillanos.edu.co>
2021-05-19 23:06:04 -0500

    yeah

--- clasificaciones/general.php
+++ clasificaciones/general.php
@@ -38,15 +38,7 @@
 
     ?>
     
-    <footer><p><br>Creado por:</p>
-        <p>Juan David Torres Barreto - 160004330</p>
-        <p>Daniel Camilo Alferez Garcia - 160004302</p>
-        <p id="copy">© 2021</p>
-        <a href="https://www.unillanos.edu.co/"><img id="unillanos" src="https://posgrados.unillanos.edu.co/maestria-estudios-culturales/images/Logo%20Unillanos2019.png" width="200px"></a>
-        <a href="https://www.instagram.com/letourdefrance/?hl=es-la"><img id="inst" src="https://imagenes.milenio.com/1udt1di_SAd03sjMqZeC9bQ7ePU=/958x596/https://www.milenio.com/uploads/media/2019/10/01/como-puedo-activar-el-modo.png" width="60px"></a>
-        <a href="https://es-la.facebook.com/letour/"><img id="face" src="https://static1.elcorreo.com/www/multimedia/202004/23/media/cortadas/1565689109_969444_1565689520_noticia_normal-k3LD-U10010140614850aB-1248x770@El%20Correo.jpg" width="55px;"></a>
-        <a href="https://twitter.com/letour"><img id="twitter" src="https://1000marcas.net/wp-content/uploads/2019/11/S%C3%ADmbolo-twitter.jpg" width="40px"></a>
-    </footer>
+    <?php require '../footer.php' ?>
     
 </body>
     
