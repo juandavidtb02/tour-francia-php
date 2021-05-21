26db38abdeb5c0151a464f4bb06f2dfad041cefd
root <dcalferez@unillanos.edu.co>
2021-05-21 18:54:06 -0500

    yeah

--- equipos/equiposParticipantes.php
+++ equipos/equiposParticipantes.php
@@ -20,32 +20,43 @@
     
     <h1><br>EQUIPOS PARTICIPANTES</h1>
 
-    <section></section>
+    <div class="buscador">
+        <form action="equiposParticipantes.php" method="GET" class="formulario">
+        <input class="texto-ingreso" type="search" name="valor" placeholder="Buscar por pais" autocomplete="off">
+       <input class="img-buscador" type="image" src="https://image.flaticon.com/icons/png/128/2932/2932802.png">
+        </form>
+    </div>
 
     <?php
         $conexion = conectarbase();
-        $query="select * from equipos order by cod_equipo";
-        $resultado=pg_query($conexion,$query) or die ("Error en consultar universidad");
-        $nr=pg_num_rows($resultado);
-        if($nr>0){
-            echo "<table align=center>
-                      <thead><td id=iz>Codigo del equipo</td><td>Nombre del equipo</td><td id=der>Pais</td></thead>";
-            while($filas=pg_fetch_array($resultado)){
-                echo "<tr><td>".$filas["cod_equipo"]."</td>";
-                echo "<td>".$filas["nomb_equipo"]."</td>";
-                echo "<td id=der>".$filas["pais_equipo"]."</td>";
-            }echo "</table>";
+        if(isset($_GET["valor"]) && $_GET["valor"] != ""){
+            $valor = $_GET["valor"];
+                $query="select cod_equipo, nomb_equipo, pais_equipo from equipos inner join pais on equipos.pais_equipo=pais.cod_pais where UNACCENT(nomb_pais) ilike '%$valor%' or pais_equipo ilike '%$valor%'";
         }else{
-            echo "No hay datos ingresados";
+            $query="select * from equipos";
         }
 
+        $resultado=pg_query($conexion,$query);
+
+        if(!$resultado or pg_num_rows($resultado)==0){
+            echo '<p  style="font-size:30px; tex height:80px; color: white; text-align:center; font-family: Arial, Helvetica, sans-serif; ">Ingresa una busqueda nuevamente</p>';
+            $resultado=pg_query($conexion,"select * from equipos") or die("Error");
+        }
+
+        echo "<table align=center>
+        <thead><td id=iz>Codigo del equipo</td><td>Nombre del equipo</td><td id=der>Pais</td></thead>";
+    while($filas=pg_fetch_array($resultado)){
+    echo "<tr><td>".$filas["cod_equipo"]."</td>";
+  echo "<td>".$filas["nomb_equipo"]."</td>";
+  echo "<td id=der>".$filas["pais_equipo"]."</td>";
+}echo "</table>";
+
     ?>
-    
+
+
     <?php require '../footer.php' ?>
     
 </body>
-    
-    
     
     
 </html>
--- etapas/recorrido.php
+++ etapas/recorrido.php
@@ -24,25 +24,54 @@
     
     <?php
         $conexion = conectarbase();
-        $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad order by cod_etapa";
-        $resultado=pg_query($conexion,$query) or die ("Error en consultar base");
-        $nr=pg_num_rows($resultado);
-        if($nr>0){
-            echo "<center><h2>Tabla etapas</h2></center>";
-            echo "<table align=center>
-                      <thead><td id=iz>Codigo</td><td>Distancia (km)</td><td>Fecha</td><td>Ciudad origen</td><td>Ciudad destino</td><td id=der>Tipo</td></thead>";
-            while($filas=pg_fetch_array($resultado)){
-                echo "<tr><td>".$filas["cod_etapa"]."</td>";
-                echo "<td>".$filas["distancia"]."</td>";
-                echo "<td>".$filas["fecha_etapa"]."</td>";
-                echo "<td>".$filas["origen"]."</td>";
-                echo "<td>".$filas["destino"]."</td>";
-                echo "<td id=der>".$filas["tipo"]."</td>";
-            }echo "</table>";
+        
+    echo "<center><h2>Tabla etapas</h2></center><table align=center>
+    <thead><td id=iz>Codigo</td><td>Distancia (km)</td><td>Fecha</td><td>Ciudad origen</td><td>Ciudad destino</td>
+    <td id=der><form action='recorrido.php' method='GET' class='filtro'>
+    <select name='tipo' class='seleccion'>
+    <option hidden selected value='Tipo'>Tipo</option>
+    <option value='Todos'>Todos</option>
+    <option value='Montaña'>Montaña</option>
+    <option value='Media Montaña'>Media Montaña</option>
+    <option value='Llana'>Llana</option>
+    <option value='Contrarreloj'>Contrarreloj</option> 
+    </select>
+    <input class='img-buscador' type='image' src='https://cdn.pixabay.com/photo/2012/04/11/00/06/filter-27229__340.png'>
+    </form></td>
+    </thead>";
+
+        if(isset($_GET["tipo"])){
+            $tipo = $_GET["tipo"];
+            if($tipo == "Todos" or $tipo == "Tipo"){
+                $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad";
+            }
+            if($tipo == "Montaña"){
+                $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad where tipo='MONTAÑA'";
+            }
+            if($tipo == "Media Montaña"){
+                $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad where tipo='MEDIA MONTAÑA'";
+            }
+            if($tipo == "Llana"){
+                $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad where tipo='LLANA'";
+            }
+            if($tipo == "Contrarreloj"){
+                $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad where tipo='CONTRARRELOJ INDIVIDUAL'";
+            }
         }else{
-            echo "No hay datos ingresados";
+            $query="select cod_etapa,distancia,fecha_etapa,c.nombre_ciudad as destino,d.nombre_ciudad as origen,tipo from ciudad c inner join etapa on c.cod_ciudad=ciudad_destino inner join ciudad d on ciudad_origen=d.cod_ciudad order by cod_etapa";
         }
 
+        $resultado=pg_query($conexion,$query) or die ("Error en consultar base");
+
+        while($filas=pg_fetch_array($resultado)){
+            echo "<tr><td>".$filas["cod_etapa"]."</td>";
+            echo "<td>".$filas["distancia"]."</td>";
+            echo "<td>".$filas["fecha_etapa"]."</td>";
+            echo "<td>".$filas["origen"]."</td>";
+            echo "<td>".$filas["destino"]."</td>";
+            echo "<td id=der>".$filas["tipo"]."</td>";
+        }echo "</table>";
+        
     ?>
 
 
