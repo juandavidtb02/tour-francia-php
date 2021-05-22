b5489346cf936f1ee692f5842a1da9161c0a8141
root <jdtorres@unillanos.edu.co>
2021-05-22 16:21:38 -0500

    make it better

--- index.php
+++ index.php
@@ -24,28 +24,33 @@
         </p>
     </div></center>
     
-    
-    <section id="cuadroTour">
+    <center>
+    <section>
         <img id="imgTour" src="https://imagenes.elpais.com/resizer/6aCXXj1jPg3bddyUtwZER5_mgu0=/394x295/cloudfront-eu-central-1.images.arcpublishing.com/prisa/FUQGTGBAURBN5GSP2ZU4QMTQF4.jpg">
-        <a href="curiosidadesTour.php"><div id="cuadrito1">Más sobre el tour</div></a>
+        <p>¡Conoce más datos interesantes<br> sobre el Tour!</p>
+        <a href="curiosidadesTour.php"><div id="cuadrito">Más sobre el tour</div></a>
     </section>
-    <section id="cuadroClasificaciones">
+    <section>
         <img id="imgClasificaciones" src="https://cadenaser00.epimg.net/ser/imagenes/2007/09/20/deportes/1190244492_740215_0000000000_noticia_normal.jpg">
-        <a href="clasificaciones/general.php"><div id="cuadrito2">Clasificaciones</div></a>
+        <p>¡Mira las clasificaciones<br> de los deportistas!</p>
+        <a href="clasificaciones/general.php"><div id="cuadrito">Clasificaciones</div></a>
     </section>
-    <section id="cuadroAboutus">
+    <section>
         <img id="imgAbout" src="https://ciclismoepico.com/wp-content/uploads/2020/12/tour-de-francia-2021-recorrido-favorito-y-perfiles-de-etapa-I.jpg">
-        <a href="infoPag.php"><div id="cuadrito3">Sobre nosotros</div></a>
+        <p>¡Conoce más sobre<br> nuestra página!</p>
+        <a href="infoPag.php"><div id="cuadrito">Sobre nosotros</div></a>
     </section>
+        </center>
+    
 
     <div class="boxLogin">
         <img src="https://cdn.pixabay.com/photo/2016/01/19/16/49/laptop-1149412_960_720.jpg">
         <?php if(!empty($user)): ?>
             <h2>Ya has ingresado</h2>
-            <a href="./admin/user.php"><section id="boton"><p>Ir al gestor</p></section></a>
+            <a href="./admin/user.php"><div id="boton"><p>Ir al gestor</p></div></a>
         <?php else: ?>
             <h2>¿Eres administrador?</h2>
-            <a href="admin/login.php"><section id="boton"><p>¡Inicia sesión!</p></section></a>
+            <a href="admin/login.php"><div id="boton"><p>¡Inicia sesión!</p></div></a>
         <?php endif; ?>
     </div>
 </body>
