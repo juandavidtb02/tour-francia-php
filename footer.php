8a5e3f2faed1bf051c6ff1c71f3e83781d354ccd
root <jdtorres@unillanos.edu.co>
2021-05-23 15:47:08 -0500

    make it better

--- footer.php
+++ footer.php
@@ -4,11 +4,12 @@
         <p>Juan David Torres Barreto - 160004330</p>
         <p>Daniel Camilo Alferez Garcia - 160004302</p>
         <p id="copy">© 2021</p>
-        <div class="social">
         <a href="https://www.unillanos.edu.co/"><img class="unillanos" src="https://posgrados.unillanos.edu.co/maestria-estudios-culturales/images/Logo%20Unillanos2019.png"></a>
+        <div class="social">
         <a href="https://www.instagram.com/letourdefrance/?hl=es-la"><img src="https://pngimage.net/wp-content/uploads/2018/06/logo-instagram-negro-png-6.png"></a>
         <a href="https://es-la.facebook.com/letour/"><img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/facebook-icon-18-256.png"></a>
         <a href="https://twitter.com/letour"><img src="https://i1.wp.com/www.creativecommons.uy/wp-content/uploads/2020/08/twitter.png?ssl=1"></a>
         <h4>¡Siguenos!</h4>
+        
         </div>
     </footer>
