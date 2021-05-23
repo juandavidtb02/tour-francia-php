a7a211bea09f042e123e086fb8157617c65ae789
root <jdtorres@unillanos.edu.co>
2021-05-23 15:19:27 -0500

    make it better

--- footer.php
+++ footer.php
@@ -1,11 +1,14 @@
-<link rel="stylesheet" type="text/css" href="/estiloFooter.css?v=<?php echo time(); ?>" />
+<link rel="stylesheet" type="text/css" href="/tour-francia-app/estiloFooter.css?v=<?php echo time(); ?>" />
 
 <footer><p><br>Creado por:</p>
         <p>Juan David Torres Barreto - 160004330</p>
         <p>Daniel Camilo Alferez Garcia - 160004302</p>
         <p id="copy">© 2021</p>
-        <a href="https://www.unillanos.edu.co/"><img id="unillanos" src="https://posgrados.unillanos.edu.co/maestria-estudios-culturales/images/Logo%20Unillanos2019.png" width="200px"></a>
-        <a href="https://www.instagram.com/letourdefrance/?hl=es-la"><img id="inst" src="https://imagenes.milenio.com/1udt1di_SAd03sjMqZeC9bQ7ePU=/958x596/https://www.milenio.com/uploads/media/2019/10/01/como-puedo-activar-el-modo.png" width="60px"></a>
-        <a href="https://es-la.facebook.com/letour/"><img id="face" src="https://static1.elcorreo.com/www/multimedia/202004/23/media/cortadas/1565689109_969444_1565689520_noticia_normal-k3LD-U10010140614850aB-1248x770@El%20Correo.jpg" width="55px;"></a>
-        <a href="https://twitter.com/letour"><img id="twitter" src="https://1000marcas.net/wp-content/uploads/2019/11/S%C3%ADmbolo-twitter.jpg" width="40px"></a>
+        <div class="social">
+        <a href="https://www.unillanos.edu.co/"><img class="unillanos" src="https://posgrados.unillanos.edu.co/maestria-estudios-culturales/images/Logo%20Unillanos2019.png"></a>
+        <a href="https://www.instagram.com/letourdefrance/?hl=es-la"><img src="https://pngimage.net/wp-content/uploads/2018/06/logo-instagram-negro-png-6.png"></a>
+        <a href="https://es-la.facebook.com/letour/"><img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/facebook-icon-18-256.png"></a>
+        <a href="https://twitter.com/letour"><img src="https://i1.wp.com/www.creativecommons.uy/wp-content/uploads/2020/08/twitter.png?ssl=1"></a>
+        <h4>¡Siguenos!</h4>
+        </div>
     </footer>
--- header.php
+++ header.php
@@ -17,37 +17,37 @@
 ?>
 
 
-<link rel="stylesheet" type="text/css" href="/estiloHeader.css?v=<?php echo time(); ?>" />
+<link rel="stylesheet" type="text/css" href="/tour-francia-app/estiloHeader.css?v=<?php echo time(); ?>" />
 
 <header>
         <a href="/index.php"><img id="logo" src="https://www.letour.fr/img/global/logo-reversed@2x.png"></a>
         <?php if(!empty($user)): ?>
-        <a href="/admin/user.php"><img id ="user" src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_960_720.png"></a>
-        <a href="/admin/logout.php"><img id="salir" src="https://cdn.icon-icons.com/icons2/368/PNG/512/Logout_37127.png"></a>
+        <a href="/tour-francia-app/admin/user.php"><img id ="user" src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_960_720.png"></a>
+        <a href="/tour-francia-app/admin/logout.php"><img id="salir" src="https://cdn.icon-icons.com/icons2/368/PNG/512/Logout_37127.png"></a>
         <?php endif;?>
         <nav id="menu">
             <ul>
-                <li id="item"><a href="/index.php">Inicio</a></li>
+                <li id="item"><a href="/tour-francia-app/index.php">Inicio</a></li>
                 <li id="item"><a href="#">Equipos</a>
                     <ul id="desple">
-                        <li><a href="/equipos/equiposParticipantes.php">Equipos participantes</a></li>
-                        <li><a href="/equipos/ciclistas.php">Ciclistas participantes</a></li>
-                        <li><a href="/equipos/paisesParticipantes.php">Paises participantes</a></li>
+                        <li><a href="/tour-francia-app/equipos/equiposParticipantes.php">Equipos participantes</a></li>
+                        <li><a href="/tour-francia-app/equipos/ciclistas.php">Ciclistas participantes</a></li>
+                        <li><a href="/tour-francia-app/equipos/paisesParticipantes.php">Paises participantes</a></li>
                     </ul>
                 </li>
                 <li id="item"><a href="#">Clasificaciones</a>
                     <ul id="desple2">
-                        <li><a href="/clasificaciones/general.php">Clasificacion general</a></li>
-                        <li><a href="/clasificaciones/sprint.php">Clasificacion de Sprint</a></li>
-                        <li><a href="/clasificaciones/montaña.php">Clasificacion por montaña</a></li>
-                        <li><a href="/clasificaciones/puntos.php">Clasificacion por puntos</a></li>
-                        <li><a href="/clasificaciones/clasificacionEquipos.php">Clasificacion por equipos</a></li>
+                        <li><a href="/tour-francia-app/clasificaciones/general.php">Clasificacion general</a></li>
+                        <li><a href="/tour-francia-app/clasificaciones/sprint.php">Clasificacion de Sprint</a></li>
+                        <li><a href="/tour-francia-app/clasificaciones/montaña.php">Clasificacion por montaña</a></li>
+                        <li><a href="/tour-francia-app/clasificaciones/puntos.php">Clasificacion por puntos</a></li>
+                        <li><a href="/tour-francia-app/clasificaciones/clasificacionEquipos.php">Clasificacion por equipos</a></li>
                     </ul>
                 </li>
                 <li id="item"><a href="#">Etapas</a>
                     <ul id="desple3">
-                        <li><a href="/etapas/recorrido.php">Recorrido 2021</a></li>
-                        <li><a href="/etapas/ganadores.php">Ganadores por etapas</a></li>
+                        <li><a href="/tour-francia-app/etapas/recorrido.php">Recorrido 2021</a></li>
+                        <li><a href="/tour-francia-app/etapas/ganadores.php">Ganadores por etapas</a></li>
                     </ul>
                 </li>
             </ul>
