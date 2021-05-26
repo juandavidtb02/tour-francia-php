62b6f41b5d08d1d5f31fb362fff0402480ad3539
root <jdtorres@unillanos.edu.co>
2021-05-26 01:22:45 -0500

    make it better

--- admin/error.php
+++ admin/error.php
@@ -2,7 +2,7 @@
 
 <br><br><br><br><br><br><br><br>
 
-<center><h2>LA INFORMACIÓN INGRESADA AL ADMINISTRADOR DE LA BASE DE DATOS<br>
-    NO OBEDECE LAS RESTRICCIONES IMPUESTAS EN LA MISMA.<br></h3></center>
+<center><h2>LA INFORMACIÓN INGRESADA  AL ADMINISTRADOR DE LA BASE DE DATOS<br>
+    NO CUMPLE LAS RESTRICCIONES IMPUESTAS EN LA MISMA.<br></h3></center>
     <center><h3>INGRESE LA INFORMACIÓN NUEVAMENTE Y VERIFIQUE QUE SEA COMPATIBLE</h3><center>
     <a href="./user.php"><div class="regresar"><p>Regresar</p></div></a>
