9d178398af22883cdc7e12ced9ee15f2730aec4f
root <jdtorres@unillanos.edu.co>
2021-08-27 22:58:58 -0500

    make it better

--- admin/addCajas.php
+++ admin/addCajas.php
@@ -6,6 +6,7 @@
         <input type="hidden" name="tabla" value="<?php echo $tabla;?>" class="varT">
         <?php while($filas = pg_fetch_array($result)):?>
             <?php if($filas['column_name'] === 'fecha_etapa' || $filas['column_name'] === 'fech_nac'):?>
+                <p><?php echo $filas['column_name'] ?>:</p>
                 <input type="date" name="<?php echo $filas['column_name'];?>" placeholder="Digite el <?php echo $filas['column_name']; ?>" autocomplete="off">
             <?php else:?>
                 <input type="text" name="<?php echo $filas['column_name'];?>" placeholder="Digite el <?php echo $filas['column_name']; ?>" autocomplete="off">
