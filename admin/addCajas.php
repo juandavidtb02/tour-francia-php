21fce4091c93a43b0fdef415b8b1377c28700a4f
root <jdtorres@unillanos.edu.co>
2021-08-31 22:54:53 -0500

    make it better

--- admin/addCajas.php
+++ admin/addCajas.php
@@ -6,8 +6,9 @@
         <input type="hidden" name="tabla" value="<?php echo $tabla;?>" class="varT">
         <?php while($filas = pg_fetch_array($result)):?>
             <?php if($filas['column_name'] === 'fecha_etapa' || $filas['column_name'] === 'fech_nac'):?>
-                <p><?php echo $filas['column_name'] ?>:</p>
+                <p id="text_date"><?php echo $filas['column_name'] ?>:</p>
                 <input type="date" name="<?php echo $filas['column_name'];?>" placeholder="Digite el <?php echo $filas['column_name']; ?>" autocomplete="off">
+                
             <?php else:?>
                 <input type="text" name="<?php echo $filas['column_name'];?>" placeholder="Digite el <?php echo $filas['column_name']; ?>" autocomplete="off">
             <?php endif;?>
@@ -15,3 +16,4 @@
         <input type="submit" name="users" value="Agregar">
     </form>
 </section>
+
--- equipos/ciclistas.php
+++ equipos/ciclistas.php
@@ -33,22 +33,22 @@
         if(isset($_GET["valor"]) && $_GET["valor"] != "" && isset($_GET["tipo"])){
             $valor = $_GET["valor"];
             if($_GET["tipo"] == "Codigo"){
-                $query="select * from ciclistas where cod_ciclista=$valor";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas where cod_ciclista=$valor group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Nombre"){
-                $query="select * from ciclistas where nomb_ciclista ilike '%$valor%'";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas where nomb_ciclista ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Apellido"){
-                $query="select * from ciclistas where apellido_ciclista ilike '%$valor%'";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas where apellido_ciclista ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Edad"){
-                $query="select * from ciclistas where date_part('year',age(current_date,fech_nac))=$valor";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas where date_part('year',age(current_date,fech_nac))=$valor group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Nacionalidad"){
-                $query="select cod_ciclista, nomb_ciclista, apellido_ciclista, fech_nac, pais_ciclista from ciclistas inner join pais on ciclistas.pais_ciclista=pais.cod_pais where unaccent(nomb_pais) ilike '%$valor%' or pais_ciclista ilike '%$valor%' or nomb_pais ilike '%$valor%'";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas inner join pais on ciclistas.pais_ciclista=pais.cod_pais where unaccent(nomb_pais) ilike '%$valor%' or pais_ciclista ilike '%$valor%' or nomb_pais ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
         }else{
-            $query="select * from ciclistas";
+            $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista order by cod_ciclista";
         }
 
         $resultado=pg_query($conexion,$query);
@@ -59,12 +59,12 @@
         }
 
             echo "<table align=center>
-                      <thead><td id=iz>Codigo del ciclista</td><td>Nombre del ciclistas</td><td>Apellido del ciclista</td><td>Fecha de nacimiento</td><td id=der>Nacionalidad</td></thead>";
+                      <thead><td id=iz>Codigo del ciclista</td><td>Nombre del ciclistas</td><td>Apellido del ciclista</td><td>Edad</td><td id=der>Nacionalidad</td></thead>";
             while($filas=pg_fetch_array($resultado)){
                 echo "<tr><td>".$filas["cod_ciclista"]."</td>";
                 echo "<td>".$filas["nomb_ciclista"]."</td>";
                 echo "<td>".$filas["apellido_ciclista"]."</td>";
-                echo "<td>".$filas["fech_nac"]."</td>";
+                echo "<td>".$filas["edad"]."</td>";
                 echo "<td id=der>".$filas["pais_ciclista"]."</td>";
             }echo "</table>";
 
