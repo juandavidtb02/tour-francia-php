e1ed4bbe02b3d1ab508b2476c21016d56b86c34b
root <dcalferez@unillanos.edu.co>
2021-09-04 00:26:55 -0500

    d

--- equipos/ciclistasRetirados.php
+++ equipos/ciclistasRetirados.php
@@ -33,22 +33,22 @@
         if(isset($_GET["valor"]) && $_GET["valor"] != "" && isset($_GET["tipo"])){
             $valor = $_GET["valor"];
             if($_GET["tipo"] == "Codigo"){
-                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas_retirados where cod_ciclista=$valor group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista, nom_equipo, inicio_contrato, fin_contrato, cant_etapas from ciclistas_retirados where cod_ciclista=$valor group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Nombre"){
-                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas_retirados where nomb_ciclista ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista, nom_equipo, inicio_contrato, fin_contrato, cant_etapas from ciclistas_retirados where nomb_ciclista ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Apellido"){
-                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas_retirados where apellido_ciclista ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista, nom_equipo, inicio_contrato, fin_contrato, cant_etapas from ciclistas_retirados where apellido_ciclista ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Edad"){
-                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas_retirados where date_part('year',age(current_date,fech_nac))=$valor group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista, nom_equipo, inicio_contrato, fin_contrato, cant_etapas from ciclistas_retirados where date_part('year',age(current_date,fech_nac))=$valor group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
             if($_GET["tipo"] == "Nacionalidad"){
-                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas_retirados inner join pais on ciclistas_retirados.pais_ciclista=pais.cod_pais where unaccent(nomb_pais) ilike '%$valor%' or pais_ciclista ilike '%$valor%' or nomb_pais ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista, nom_equipo, inicio_contrato, fin_contrato, cant_etapas from ciclistas_retirados inner join pais on ciclistas_retirados.pais_ciclista=pais.cod_pais where unaccent(nomb_pais) ilike '%$valor%' or pais_ciclista ilike '%$valor%' or nomb_pais ilike '%$valor%' group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista";
             }
         }else{
-                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista from ciclistas_retirados group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista order by cod_ciclista";
+                $query="select cod_ciclista,nomb_ciclista,apellido_ciclista,(current_date - fech_nac)/365 as edad,pais_ciclista, nom_equipo, inicio_contrato, fin_contrato, cant_etapas from ciclistas_retirados group by cod_ciclista,nomb_ciclista,apellido_ciclista,edad,pais_ciclista order by cod_ciclista";
         }
 
         $resultado=pg_query($conexion,$query);
@@ -59,13 +59,18 @@
         }
 
             echo "<table align=center>
-                      <thead><td id=iz>Codigo del ciclista</td><td>Nombre del ciclistas</td><td>Apellido del ciclista</td><td>Edad</td><td id=der>Nacionalidad</td></thead>";
+                      <thead><td id=iz>Codigo del ciclista</td><td>Nombre del ciclistas</td><td>Apellido del ciclista</td><td>Edad</td><td>Nacionalidad</td><td>Equipo</td><td>Inicio Contrato</td><td>Fin Contrato</td>Etapas Completadas<td id=der></td></thead>";
             while($filas=pg_fetch_array($resultado)){
                 echo "<tr><td>".$filas["cod_ciclista"]."</td>";
                 echo "<td>".$filas["nomb_ciclista"]."</td>";
                 echo "<td>".$filas["apellido_ciclista"]."</td>";
                 echo "<td>".$filas["edad"]."</td>";
-                echo "<td id=der>".$filas["pais_ciclista"]."</td>";
+                echo "<td>".$filas["pais_ciclista"]."</td>";
+                echo "<td>".$filas["nomb_equipo"]."</td>";
+                echo "<td>".$filas["inicio_contrato"]."</td>";
+                echo "<td>".$filas["fin_contrato"]."</td>";
+                echo "<td id=der>".$filas["cant_etapas"]."</td>";
+
             }echo "</table>";
 
     ?>
